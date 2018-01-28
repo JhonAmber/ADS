42947cbd651c0016ecabff07385d4879b899849a
Jhon Esteban <31297855+JhonAmber@users.noreply.github.com>
2018-01-28 18:22:03 -0500

    web socket
    chat

---
+++ public/js/websocket.js
@@ -0,0 +1,40 @@
+
+(function (window,document,JSON){
+          
+          var url = 'ws://echo.websocket.org/',
+                           ws = new WebSocket(url),
+                           btnEnviar = document.getElementById("enviar"),
+                           mensaje = document.getElementById("mensaje"),
+                           resultado = document.getElementById("resultado");
+                          
+          ws.onopen = onOpen;
+          ws.onclose = onClose;
+          ws.onmessage = onMessage;
+
+          btnEnviar.addEventListener('click',Enviar);
+          
+          function onOpen(){
+                console.log("conectado........") ;
+          }
+
+          function onClose(){
+                    console.log('desconectado.....');
+          }
+
+          function onMessage(evt){
+
+                var obj = JSON.parse(evt.data),
+                msg = obj.mensaje;  
+
+                resultado.innerHTML += "<br>" + msg;
+          }
+
+
+          function Enviar(){
+
+                var msg={ mensaje: mensaje.value };       
+                ws.send(JSON.stringify(msg));
+          }
+          
+})(window,document,JSON);
+
